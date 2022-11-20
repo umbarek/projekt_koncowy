99c99742d230a7b405ff363f88f8e1e70a5d5d16
Tomasz Pograniczny <tpograniczny@gmail.com>
2022-11-20 11:33:23 +0100

    edit option added

--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, request
 from blog import app
 from blog.models import Entry, db
+from blog.forms import EntryForm
 
 
 @app.route("/")
@@ -8,3 +9,32 @@ def index():
       all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
       return render_template("homepage.html",  all_posts=all_posts)
 
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+   form = EntryForm()
+   errors = None
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           entry = Entry(
+               title=form.title.data,
+               body=form.body.data,
+               is_published=form.is_published.data
+           )
+           db.session.add(entry)
+           db.session.commit()
+       else:
+           errors = form.errors
+   return render_template("entry_form.html", form=form, errors=errors)
+
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def edit_entry(entry_id):
+   entry = Entry.query.filter_by(id=entry_id).first_or_404()
+   form = EntryForm(obj=entry)
+   errors = None
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           form.populate_obj(entry)
+           db.session.commit()
+       else:
+           errors = form.errors
+   return render_template("entry_form.html", form=form, errors=errors)
